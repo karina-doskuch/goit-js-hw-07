dc642fafe83322607eb16c5aea78ca09a3cd25f7
Karina Doskuch <karina.doskuch@gmail.com>
2022-10-12 20:36:59 +0300

    Task1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const basicLightbox = window.basicLightbox;
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </div>
+            `;
+    })
+    .join("");
+}
+
+const galleryRef = document.querySelector("div.gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryRef.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+galleryRef.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const url = event.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `
+        <img src="${url}">
+        `
+  );
+  instance.show();
+  console.log(url);
+}
