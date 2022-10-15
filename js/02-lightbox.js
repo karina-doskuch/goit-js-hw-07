1aaa7bdc3b34065e4348dd592e9a907ccb618c92
Karina Doskuch <karina.doskuch@gmail.com>
2022-10-15 20:46:08 +0300

    Task2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+                  <a class="gallery__item" href="${original}">
+                      <img
+                          class="gallery__image"
+                          src="${preview}"
+                          alt="${description}"
+                      />
+                  </a>
+              `;
+    })
+    .join("");
+}
+
+const galleryRef = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    lightbox.close();
+  }
+});
